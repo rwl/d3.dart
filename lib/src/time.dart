244b2d7281f7b8931649a3882585c8b5302ca5a8
Richard Lincoln <r.w.lincoln@gmail.com>
2015-12-07 21:50:55 +0100

    Implementing Dart API for `time` library.

--- lib/src/time.dart
+++ lib/src/time.dart
@@ -1,12 +1,13 @@
 library d3.src.time;
 
 import 'dart:js';
+import 'd3.dart';
 
 JsObject _time = context['d3']['time'];
 
 /// Create a new local time formatter for a given specifier.
-Format format(specifier) {
-  return _time.callMethod('format', []);
+Format format(String specifier) {
+  return new Format._(_time.callMethod('format', [specifier]));
 }
 
 class Format {
@@ -14,335 +15,398 @@ class Format {
 
   Format._(this._proxy);
 
-  call(date) => format(date);
-
   /// Format a date into a string.
-  format(date) {
-    return _proxy.callMethod('format', []);
-  }
+  String call(DateTime date) => _proxy.callMethod('call', [_proxy, date]);
 
   /// Parse a string into a date.
-  parse(string) {
-    return _proxy.callMethod('parse', []);
-  }
+  DateTime parse(String string) => _proxy.callMethod('parse', [string]);
 }
 
 /// Create a new local multi-resolution time formatter.
-multi(formats) {
-  return _time['format'].callMethod('multi', []);
+Format multi(List formats) {
+  return new Format._(_time['format'].callMethod('multi', [formats]));
 }
 
 /// Create a new UTC time formatter for a given specifier.
-utcFormat(specifier) {
-  return _time['format'].callMethod('utc', []);
+Format utcFormat(String specifier) {
+  return new Format._(_time['format'].callMethod('utc', [specifier]));
 }
 
 /// The ISO 8601 UTC time formatter.
-Format get iso {
-  return _time['format'].callMethod('iso', []);
-}
+Format get iso => new Format._(_time['format']['iso']);
 
 /// Construct a linear time scale.
-TimeScale scale() {
-  return _time.callMethod('scale', []);
-}
+TimeScale scale() => new TimeScale._(_time.callMethod('scale'));
 
-TimeScale utcScale() {
-  return _time['scale'].callMethod('utc', []);
-}
+TimeScale utcScale() => new TimeScale._(_time['scale'].callMethod('utc'));
 
 class TimeScale {
   final JsObject _proxy;
 
   TimeScale._(this._proxy);
 
-  call(x) => scale(x);
-
   /// Get the range value corresponding to a given domain value.
-  scale(x) {
-    return _proxy.callMethod('scale', []);
-  }
+  call(x) => _proxy.callMethod('call', [_proxy, x]);
 
   /// Get the domain value corresponding to a given range value.
-  invert(y) {
-    return _proxy.callMethod('invert', []);
-  }
+  invert(y) => _proxy.callMethod('invert', [y]);
 
   /// Get or set the scale's input domain.
-  domain([dates]) {
-    return _proxy.callMethod('domain', []);
+  domain([List dates = undefined]) {
+    var args = [];
+    if (dates != undefined) {
+      args.add(dates);
+    }
+    var retval = _proxy.callMethod('domain', args);
+    if (dates == undefined) {
+      return retval;
+    } else {
+      return new TimeScale._(retval);
+    }
   }
 
   /// Extend the scale domain to nice round numbers.
-  nice([interval_or_count, step]) {
-    return _proxy.callMethod('nice', []);
+  TimeScale nice([interval_or_count = undefined, step = undefined]) {
+    var args = [];
+    if (interval_or_count != undefined) {
+      args.add(interval_or_count);
+    }
+    if (step != undefined) {
+      args.add(step);
+    }
+    return new TimeScale._(_proxy.callMethod('nice', args));
   }
 
   /// Get or set the scale's output range.
-  range([values]) {
-    return _proxy.callMethod('range', []);
+  range([List values = undefined]) {
+    var args = [];
+    if (values != undefined) {
+      args.add(values);
+    }
+    var retval = _proxy.callMethod('range', args);
+    if (values == undefined) {
+      return retval;
+    } else {
+      return new TimeScale._(retval);
+    }
   }
 
   /// Set the scale's output range, and enable rounding.
-  rangeRound([values]) {
-    return _proxy.callMethod('rangeRound', []);
+  rangeRound([List values = undefined]) {
+    var args = [];
+    if (values != undefined) {
+      args.add(values);
+    }
+    var retval = _proxy.callMethod('rangeRound', args);
+    if (values == undefined) {
+      return retval;
+    } else {
+      return new TimeScale._(retval);
+    }
   }
 
   /// Get or set the scale's output interpolator.
-  interpolate([factory]) {
-    return _proxy.callMethod('interpolate', []);
+  interpolate([Function factory = undefined]) {
+    var args = [];
+    if (factory != undefined) {
+      args.add(factory);
+    }
+    var retval = _proxy.callMethod('interpolate', args);
+    if (factory == undefined) {
+      return retval;
+    } else {
+      return new TimeScale._(retval);
+    }
   }
 
   /// Enable or disable clamping of the output range.
-  clamp([boolean]) {
-    return _proxy.callMethod('clamp', []);
+  clamp([bool boolean = undefined]) {
+    var args = [];
+    if (boolean != undefined) {
+      args.add(boolean);
+    }
+    var retval = _proxy.callMethod('clamp', args);
+    if (boolean == undefined) {
+      return retval;
+    } else {
+      return new TimeScale._(retval);
+    }
   }
 
   /// Get representative values from the input domain.
-  ticks([interval_or_count, step]) {
-    return _proxy.callMethod('ticks', []);
+  ticks([interval_or_count = undefined, num step = undefined]) {
+    var args = [];
+    if (interval_or_count != undefined) {
+      args.add(interval_or_count);
+    }
+    if (step != undefined) {
+      args.add(step);
+    }
+    return _proxy.callMethod('ticks', args);
   }
 
   /// Get a formatter for displaying tick values.
-  tickFormat() {
-    return _proxy.callMethod('tickFormat', []);
-  }
+  Function tickFormat() => _proxy.callMethod('tickFormat');
 
   /// Create a new scale from an existing scale.
-  copy() {
-    return _proxy.callMethod('copy', []);
-  }
+  TimeScale copy() => new TimeScale._(_proxy.callMethod('copy'));
 }
 
 /// A time interval in local time.
-Interval interval() {
-  return _time.callMethod('interval', []);
-}
+Interval interval() => new Interval._(_time.callMethod('interval'));
 
 class Interval {
   final JsObject _proxy;
 
   Interval._(this._proxy);
 
-  call(date) => interval(date);
-
   /// Alias for interval.floor.
-  interval(date) {
-    return _proxy.callMethod('interval', []);
+  DateTime call(DateTime date) {
+    return _proxy.callMethod('call', [_proxy, date]);
   }
 
   /// Rounds down to the nearest interval.
-  floor(date) {
-    return _proxy.callMethod('floor', []);
+  DateTime floor(DateTime date) {
+    return _proxy.callMethod('floor', [date]);
   }
 
   /// Rounds up or down to the nearest interval.
-  round(date) {
-    return _proxy.callMethod('round', []);
+  DateTime round(DateTime date) {
+    return _proxy.callMethod('round', [date]);
   }
 
   /// Rounds up to the nearest interval.
-  ceil(date) {
-    return _proxy.callMethod('ceil', []);
+  DateTime ceil(DateTime date) {
+    return _proxy.callMethod('ceil', [date]);
   }
 
   /// Returns dates within the specified range.
-  range(start, stop, [step]) {
-    return _proxy.callMethod('range', []);
+  range(start, stop, [num step = undefined]) {
+    var args = [start, stop];
+    if (step != undefined) {
+      args.add(step);
+    }
+    return _proxy.callMethod('range', args);
   }
 
   /// Returns a date offset by some interval.
-  offset(date, step) {
-    return _proxy.callMethod('offset', []);
+  DateTime offset(DateTime date, num step) {
+    return _proxy.callMethod('offset', [date, step]);
   }
 
   /// Returns the UTC-equivalent time interval.
-  get utc {
-    return _proxy['utc'];
-  }
+  Interval get utc => new Interval._(_proxy['utc']);
 }
 
 /// Every second (e.g., 1:02:03 AM).
-Interval get second {
-  return _time['second'];
-}
+Interval get second => new Interval._(_time['second']);
 
 /// Every minute (e.g., 1:02 AM).
-Interval get minute {
-  return _time['minute'];
-}
+Interval get minute => new Interval._(_time['minute']);
 
 /// Every hour (e.g., 1:00 AM).
-Interval get hour {
-  return _time['hour'];
-}
+Interval get hour => new Interval._(_time['hour']);
 
 /// Every day (12:00 AM).
-Interval get day {
-  return _time['day'];
-}
+Interval get day => new Interval._(_time['day']);
 
 /// Alias for sunday.
-Interval get week {
-  return _time['week'];
-}
+Interval get week => new Interval._(_time['week']);
 
 /// Every Sunday (e.g., February 5, 12:00 AM).
-Interval get sunday {
-  return _time['sunday'];
-}
+Interval get sunday => new Interval._(_time['sunday']);
 
 /// Every Monday (e.g., February 5, 12:00 AM).
-Interval get monday {
-  return _time['monday'];
-}
+Interval get monday => new Interval._(_time['monday']);
 
 /// Every Tuesday (e.g., February 5, 12:00 AM).
-Interval get tuesday {
-  return _time['tuesday'];
-}
+Interval get tuesday => new Interval._(_time['tuesday']);
 
 /// Every Wednesday (e.g., February 5, 12:00 AM).
-Interval get wednesday {
-  return _time['wednesday'];
-}
+Interval get wednesday => new Interval._(_time['wednesday']);
 
 /// Every Thursday (e.g., February 5, 12:00 AM).
-Interval get thursday {
-  return _time['thursday'];
-}
+Interval get thursday => new Interval._(_time['thursday']);
 
 /// Every Friday (e.g., February 5, 12:00 AM).
-Interval get friday {
-  return _time['friday'];
-}
+Interval get friday => new Interval._(_time['friday']);
 
 /// Every Saturday (e.g., February 5, 12:00 AM).
-Interval get saturday {
-  return _time['saturday'];
-}
+Interval get saturday => new Interval._(_time['saturday']);
 
 /// Every month (e.g., February 1, 12:00 AM).
-Interval get month {
-  return _time['month'];
-}
+Interval get month => new Interval._(_time['month']);
 
 /// Every year (e.g., January 1, 12:00 AM).
-Interval get year {
-  return _time['year'];
-}
+Interval get year => new Interval._(_time['year']);
 
 /// Alias for second.range.
-seconds(start, stop, [step]) {
-  return _time.callMethod('seconds', []);
+List seconds(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('seconds', args);
 }
 
 /// Alias for minute.range.
-minutes(start, stop, [step]) {
-  return _time.callMethod('minutes', []);
+List minutes(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('minutes', args);
 }
 
 /// Alias for hour.range.
-hours(start, stop, [step]) {
-  return _time.callMethod('hours', []);
+List hours(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('hours', args);
 }
 
 /// Alias for day.range.
-days(start, stop, [step]) {
-  return _time.callMethod('days', []);
+List days(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('days', args);
 }
 
 /// Alias for sunday.range.
-weeks(start, stop, [step]) {
-  return _time.callMethod('weeks', []);
+List weeks(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('weeks', args);
 }
 
 /// Alias for sunday.range.
-sundays(start, stop, [step]) {
-  return _time.callMethod('sundays', []);
+List sundays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('sundays', args);
 }
 
 /// Alias for monday.range.
-mondays(start, stop, [step]) {
-  return _time.callMethod('mondays', []);
+List mondays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('mondays', args);
 }
 
 /// Alias for tuesday.range.
-tuesdays(start, stop, [step]) {
-  return _time.callMethod('tuesdays', []);
+List tuesdays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('tuesdays', args);
 }
 
 /// Alias for wednesday.range.
-wednesdays(start, stop, [step]) {
-  return _time.callMethod('wednesdays', []);
+List wednesdays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('wednesdays', args);
 }
 
 /// Alias for thursday.range.
-thursdays(start, stop, [step]) {
-  return _time.callMethod('thursdays', []);
+List thursdays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('thursdays', args);
 }
 
 /// Alias for friday.range.
-fridays(start, stop, [step]) {
-  return _time.callMethod('fridays', []);
+List fridays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('fridays', args);
 }
 
 /// Alias for saturday.range.
-saturdays(start, stop, [step]) {
-  return _time.callMethod('saturdays', []);
+List saturdays(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('saturdays', args);
 }
 
 /// Alias for month.range.
-months(start, stop, [step]) {
-  return _time.callMethod('months', []);
+List months(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('months', args);
 }
 
 /// Alias for year.range.
-years(start, stop, [step]) {
-  return _time.callMethod('years', []);
+List years(start, stop, [num step = undefined]) {
+  var args = [start, stop];
+  if (step != undefined) {
+    args.add(step);
+  }
+  return _time.callMethod('years', args);
 }
 
 /// Computes the day number.
-dayOfYear(date) {
-  return _time.callMethod('dayOfYear', []);
+int dayOfYear(DateTime date) {
+  return _time.callMethod('dayOfYear', [date]);
 }
 
 /// Alias for sundayOfYear.
-weekOfYear(date) {
-  return _time.callMethod('weekOfYear', []);
+int weekOfYear(DateTime date) {
+  return _time.callMethod('weekOfYear', [date]);
 }
 
 /// Computes the sunday-based week number.
-sundayOfYear(date) {
-  return _time.callMethod('sundayOfYear', []);
+int sundayOfYear(DateTime date) {
+  return _time.callMethod('sundayOfYear', [date]);
 }
 
 /// Computes the monday-based week number.
-mondayOfYear(date) {
-  return _time.callMethod('mondayOfYear', []);
+int mondayOfYear(DateTime date) {
+  return _time.callMethod('mondayOfYear', [date]);
 }
 
 /// Computes the tuesday-based week number.
-tuesdayOfYear(date) {
-  return _time.callMethod('tuesdayOfYear', []);
+int tuesdayOfYear(DateTime date) {
+  return _time.callMethod('tuesdayOfYear', [date]);
 }
 
 /// Computes the wednesday-based week number.
-wednesdayOfYear(date) {
-  return _time.callMethod('wednesdayOfYear', []);
+int wednesdayOfYear(DateTime date) {
+  return _time.callMethod('wednesdayOfYear', [date]);
 }
 
 /// Computes the thursday-based week number.
-thursdayOfYear(date) {
-  return _time.callMethod('thursdayOfYear', []);
+int thursdayOfYear(DateTime date) {
+  return _time.callMethod('thursdayOfYear', [date]);
 }
 
 /// Computes the friday-based week number.
-fridayOfYear(date) {
-  return _time.callMethod('fridayOfYear', []);
+int fridayOfYear(DateTime date) {
+  return _time.callMethod('fridayOfYear', [date]);
 }
 
 /// Computes the saturday-based week number.
-saturdayOfYear(date) {
-  return _time.callMethod('saturdayOfYear', []);
+int saturdayOfYear(DateTime date) {
+  return _time.callMethod('saturdayOfYear', [date]);
 }
