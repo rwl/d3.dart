c23b7c7ffc172f95f32cf9d0b892d2e812cd29d8
Richard Lincoln <r.w.lincoln@gmail.com>
2015-12-08 14:04:19 +0100

    Implementing Dart API for `geom` and `behavior` libraries.

--- lib/src/behavior.dart
+++ lib/src/behavior.dart
@@ -1,91 +1,196 @@
 library d3.src.behavior;
 
 import 'dart:js';
+import 'd3.dart';
+
+import 'selection.dart' as sel;
+import 'scale.dart' as sc;
+import 'transition.dart' as trans;
 
 JsObject _behavior = context['d3']['behavior'];
 
-Drag drag() {
-  return _behavior.callMethod('drag', []);
-}
+Drag drag() => new Drag._(_behavior.callMethod('drag'));
 
 class Drag {
   final JsObject _proxy;
 
   Drag._(this._proxy);
 
-  Drag on(type, [listener]) {
-    return _proxy.callMethod('on', []);
+  Drag on(String type, [Function listener = undefined]) {
+    var args = [];
+    if (listener != undefined) {
+      args.add(listener);
+    }
+    var retval = _proxy.callMethod('on', args);
+    if (listener == undefined) {
+      return retval;
+    } else {
+      return new Drag._(retval);
+    }
   }
 
-  Drag origin([origin]) {
-    return _proxy.callMethod('origin', []);
+  Drag origin([Function origin = undefined]) {
+    var args = [];
+    if (origin != undefined) {
+      args.add(origin);
+    }
+    var retval = _proxy.callMethod('origin', args);
+    if (origin == undefined) {
+      return retval;
+    } else {
+      return new Drag._(retval);
+    }
   }
 }
 
 /// Create a zoom behavior.
-Zoom zoom() {
-  return _behavior.callMethod('zoom', []);
-}
+Zoom zoom() => new Zoom._(_behavior.callMethod('zoom'));
 
 class Zoom {
   final JsObject _proxy;
 
   Zoom._(this._proxy);
 
-  call(selection) => zoom(selection);
-
   /// Apply the zoom behavior to the selected elements.
-  zoom(selection) {
-    return _proxy.callMethod('zoom', []);
+  call(selection) {
+    var args = [_proxy];
+    if (selection is sel.Selection) {
+      args.add(sel.getProxy(selection));
+    } else if (selection is trans.Transition) {
+      args.add(trans.getProxy(selection));
+    } else {
+      throw new ArgumentError.value(selection);
+    }
+    return new Zoom._(_proxy.callMethod('call', args));
   }
 
   /// The current translate offset.
-  Zoom translate([translate]) {
-    return _proxy.callMethod('translate', []);
+  translate([List translate = undefined]) {
+    var args = [];
+    if (translate != undefined) {
+      args.add(translate);
+    }
+    var retval = _proxy.callMethod('translate', args);
+    if (translate == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// The current scale factor.
-  Zoom scale([scale]) {
-    return _proxy.callMethod('scale', []);
+  Zoom scale([num scale = undefined]) {
+    var args = [];
+    if (scale != undefined) {
+      args.add(scale);
+    }
+    var retval = _proxy.callMethod('scale', args);
+    if (scale == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// Optional limits on the scale factor.
-  Zoom scaleExtent([extent]) {
-    return _proxy.callMethod('scaleExtent', []);
+  Zoom scaleExtent([List extent = undefined]) {
+    var args = [];
+    if (extent != undefined) {
+      args.add(extent);
+    }
+    var retval = _proxy.callMethod('scaleExtent', args);
+    if (extent == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// An optional focal point for mousewheel zooming.
-  Zoom center([center]) {
-    return _proxy.callMethod('center', []);
+  Zoom center([List center = undefined]) {
+    var args = [];
+    if (center != undefined) {
+      args.add(center);
+    }
+    var retval = _proxy.callMethod('center', args);
+    if (center == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// The dimensions of the viewport.
-  Zoom size([size]) {
-    return _proxy.callMethod('size', []);
+  Zoom size([List size = undefined]) {
+    var args = [];
+    if (size != undefined) {
+      args.add(size);
+    }
+    var retval = _proxy.callMethod('size', args);
+    if (size == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// An optional scale whose domain is bound to the x extent of the viewport.
-  Zoom x([x]) {
-    return _proxy.callMethod('x', []);
+  x([x = undefined]) {
+    var args = [];
+    if (x != undefined) {
+      args.add(sc.getProxy(x));
+    }
+    var retval = _proxy.callMethod('x', args);
+    if (x == undefined) {
+      return retval; // TODO: wrap in Scale
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// An optional scale whose domain is bound to the y extent of the viewport.
-  Zoom y([y]) {
-    return _proxy.callMethod('y', []);
+  y([y = undefined]) {
+    var args = [];
+    if (y != undefined) {
+      args.add(sc.getProxy(y));
+    }
+    var retval = _proxy.callMethod('y', args);
+    if (y == undefined) {
+      return retval; // TODO: wrap in Scale
+    } else {
+      return new Zoom._(retval);
+    }
   }
 
   /// Listeners for when the scale or translate changes.
-  Zoom on(type, listener) {
-    return _proxy.callMethod('on', []);
+  Zoom on(String type, Function listener) {
+    return new Zoom._(_proxy.callMethod('on', [type, listener]));
   }
 
   /// Dispatch zoom events after setting the scale or translate.
   Zoom event(selection) {
-    return _proxy.callMethod('event', []);
+    var args = [];
+    if (selection is sel.Selection) {
+      args.add(sel.getProxy(selection));
+    } else if (selection is trans.Transition) {
+      args.add(trans.getProxy(selection));
+    } else {
+      throw new ArgumentError.value(selection);
+    }
+    return _proxy.callMethod('event', args);
   }
 
   /// Get or set the dblclick transition duration.
-  Zoom duration([duration]) {
-    return _proxy.callMethod('duration', []);
+  Zoom duration([num duration = undefined]) {
+    var args = [];
+    if (duration != undefined) {
+      args.add(sc.getProxy(duration));
+    }
+    var retval = _proxy.callMethod('duration', args);
+    if (duration == undefined) {
+      return retval;
+    } else {
+      return new Zoom._(retval);
+    }
   }
 }
--- lib/src/geom.dart
+++ lib/src/geom.dart
@@ -1,55 +1,72 @@
 library d3.src.geom;
 
 import 'dart:js';
+import 'd3.dart';
 
-JsObject _d3 = context['d3'];
 JsObject _geom = context['d3']['geom'];
 
 /// Create a Voronoi layout with default accessors.
-Voronoi voronoi() {
-  return _geom.callMethod('voronoi', []);
-}
+Voronoi voronoi() => new Voronoi._(_geom.callMethod('voronoi'));
 
 class Voronoi {
   final JsObject _proxy;
 
   Voronoi._(this._proxy);
 
-  call(data) => voronoi(data);
-
   /// Compute the Voronoi tessellation for the specified points.
-  voronoi(data) {
-    return _proxy.callMethod('voronoi', []);
-  }
+  List call(List data) => _proxy.callMethod('call', [_proxy, data]);
 
   /// Get or set the x-coordinate accessor for each point.
-  x([x]) {
-    return _proxy.callMethod('x', []);
+  x([Function x = undefined]) {
+    var args = [];
+    if (x != undefined) {
+      args.add(x);
+    }
+    var retval = _proxy.callMethod('x', args);
+    if (x == undefined) {
+      return retval;
+    } else {
+      return new Voronoi._(retval);
+    }
   }
 
   /// Get or set the y-coordinate accessor for each point.
-  y([y]) {
-    return _proxy.callMethod('y', []);
+  y([Function y = undefined]) {
+    var args = [];
+    if (y != undefined) {
+      args.add(y);
+    }
+    var retval = _proxy.callMethod('y', args);
+    if (y == undefined) {
+      return retval;
+    } else {
+      return new Voronoi._(retval);
+    }
   }
 
   /// Get or set the clip extent for the tesselation.
-  clipExtent([extent]) {
-    return _proxy.callMethod('clipExtent', []);
+  clipExtent([List extent = undefined]) {
+    var args = [];
+    if (extent != undefined) {
+      args.add(extent);
+    }
+    var retval = _proxy.callMethod('clipExtent', args);
+    if (extent == undefined) {
+      return retval;
+    } else {
+      return new Voronoi._(retval);
+    }
   }
 
   /// Compute the Delaunay mesh as a network of links.
-  links(data) {
-    return _proxy.callMethod('links', []);
-  }
+  List links(List data) => _proxy.callMethod('links', [data]);
 
   /// Compute the Delaunay mesh as a triangular tessellation.
-  triangles(data) {
-    return _proxy.callMethod('triangles', []);
-  }
+  List triangles(List data) => _proxy.callMethod('triangles', [data]);
 }
 
 Quadtree quadtree() {
-  return _geom.callMethod('quadtree', []);
+  return new Quadtree._(_geom.callMethod('quadtree'));
 }
 
 class Quadtree {
@@ -57,23 +74,65 @@ class Quadtree {
 
   Quadtree._(this._proxy);
 
-  call(points, [x1, y1, x2, y2]) => quadtree(points, [x1, y1, x2, y2]);
-
   /// Constructs a quadtree for an array of points.
-  Root quadtree(points, [x1, y1, x2, y2]) {
-    return _proxy.callMethod('quadtree', []);
+  Root call(List points,
+      [num x2_x1 = undefined,
+      num y2_y1 = undefined,
+      num x2 = undefined,
+      num y2 = undefined]) {
+    var args = [_proxy, points];
+    if (x2_x1 != undefined) {
+      args.add(x2_x1);
+    }
+    if (y2_y1 != undefined) {
+      args.add(y2_y1);
+    }
+    if (x2 != undefined) {
+      args.add(x2);
+    }
+    if (y2 != undefined) {
+      args.add(y2);
+    }
+    return new Root._(_proxy.callMethod('call', args));
   }
 
-  x([x]) {
-    return _proxy.callMethod('x', []);
+  x([Function x = undefined]) {
+    var args = [];
+    if (x != undefined) {
+      args.add(x);
+    }
+    var retval = _proxy.callMethod('x', args);
+    if (x == undefined) {
+      return retval;
+    } else {
+      return new Quadtree._(retval);
+    }
   }
 
-  y([y]) {
-    return _proxy.callMethod('y', []);
+  y([Function y = undefined]) {
+    var args = [];
+    if (y != undefined) {
+      args.add(y);
+    }
+    var retval = _proxy.callMethod('y', args);
+    if (y == undefined) {
+      return retval;
+    } else {
+      return new Quadtree._(retval);
+    }
   }
 
-  extent([extent]) {
-    return _proxy.callMethod('extent', []);
+  extent([List extent = undefined]) {
+    var args = [];
+    if (extent != undefined) {
+      args.add(extent);
+    }
+    var retval = _proxy.callMethod('extent', args);
+    if (extent == undefined) {
+      return retval;
+    } else {
+      return new Quadtree._(retval);
+    }
   }
 }
 
@@ -83,24 +142,20 @@ class Root {
   Root._(this._proxy);
 
   /// Add a point to the quadtree.
-  add(point) {
-    return _proxy.callMethod('add', []);
-  }
+  add(List point) => new Root._(_proxy.callMethod('add', [point]));
 
   /// Recursively visit nodes in the quadtree.
-  visit(callback) {
-    return _proxy.callMethod('visit', []);
+  visit(callback(node, x1, y1, x2, y2)) {
+    return new Root._(_proxy.callMethod('visit', [callback]));
   }
 
   /// Find the closest point in the quadtree.
-  find(point) {
-    return _proxy.callMethod('find', []);
-  }
+  List find(List point) => _proxy.callMethod('find', [point]);
 }
 
 /// Create a polygon from the specified array of points.
-Polygon polygon(vertices) {
-  return _geom.callMethod('polygon', []);
+Polygon polygon(List vertices) {
+  return new Polygon._(_geom.callMethod('polygon', [vertices]));
 }
 
 class Polygon {
@@ -109,25 +164,19 @@ class Polygon {
   Polygon._(this._proxy);
 
   /// Compute the counterclockwise area of this polygon.
-  area() {
-    return _proxy.callMethod('area', []);
-  }
+  num area() => _proxy.callMethod('area');
 
   /// Compute the area centroid of this polygon.
-  centroid() {
-    return _proxy.callMethod('centroid', []);
-  }
+  List centroid() => _proxy.callMethod('centroid');
 
   /// Clip the specified polygon to this polygon.
-  clip(subject) {
-    return _proxy.callMethod('clip', []);
+  clip(Polygon subject) {
+    return new Polygon._(_proxy.callMethod('clip', [subject]));
   }
 }
 
 /// Create a convex hull layout with default accessors.
-Hull hull() {
-  return _geom.callMethod('hull', []);
-}
+Hull hull() => new Hull._(_geom.callMethod('hull'));
 
 class Hull {
   final JsObject _proxy;
@@ -135,17 +184,33 @@ class Hull {
   Hull._(this._proxy);
 
   /// Compute the convex hull for the given array of points.
-  hull(vertices) {
-    return _proxy.callMethod('hull', []);
-  }
+  hull(List vertices) => _proxy.callMethod('hull', [vertices]);
 
   /// Get or set the x-coordinate accessor.
-  x([x]) {
-    return _proxy.callMethod('x', []);
+  x([Function x = undefined]) {
+    var args = [];
+    if (x != undefined) {
+      args.add(x);
+    }
+    var retval = _proxy.callMethod('x', args);
+    if (x == undefined) {
+      return retval;
+    } else {
+      return new Hull._(retval);
+    }
   }
 
   /// Get or set the y-coordinate accessor.
-  y([y]) {
-    return _proxy.callMethod('y', []);
+  y([Function y = undefined]) {
+    var args = [];
+    if (y != undefined) {
+      args.add(y);
+    }
+    var retval = _proxy.callMethod('y', args);
+    if (y == undefined) {
+      return retval;
+    } else {
+      return new Hull._(retval);
+    }
   }
 }
